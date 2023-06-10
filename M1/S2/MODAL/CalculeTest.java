d12a49ba7924dd8078f23e940f76cd472fcbfdb7
Ayoub <56415787+iyy0v@users.noreply.github.com>
2023-06-10 13:07:25 +0100

    Delete M1/S2 directory

--- M1/S2/MODAL/CalculeTest.java
+++
@@ -1,6 +0,0 @@
-import org.junit.*;
-public class CalculeTest {
-Calcule cal = new Calcule();
-@Test
-void Testsomme() {assertEquals(?,cal.somme(5,5));}
-}
--- M1/S2/MODAL/src/Pack1/CalculeTest.java
+++
@@ -1,7 +0,0 @@
-package Pack1;
-import org.junit.*;
-public class CalculeTest {
-Calcule cal = new Calcule();
-	@Test
-	void Testsomme() { assertEquals( ? ,cal.somme(?,?)); }
-}
